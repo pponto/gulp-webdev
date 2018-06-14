e75d289c7a1de5ae94d4ce8acc5a8cc78ef94680
Pedro Moreira <pedromoreira@gmail.com>
2018-06-14 18:21:05 -0300

    update comments and description

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-///////////////////////////////////////////////////
-// Required
-///////////////////////////////////////////////////
+/**
+ * Requirements
+ */
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	postcss = require('gulp-postcss'),
@@ -14,11 +14,12 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	imagemin = require('gulp-imagemin'),
 	pngquant = require('imagemin-pngquant'),
-	htmlmin = require('gulp-htmlmin')
+	htmlmin = require('gulp-htmlmin');
 
-///////////////////////////////////////////////////
-// Styles
-///////////////////////////////////////////////////
+/**
+ * Sass and PostCSS - Required by "gulp" and "gulp dist"
+ */
+// PostCSS Variables
 var processors = [
 	autoprefixer({
 		browsers: ['defaults', 'last 2 iOS major versions']
@@ -26,8 +27,8 @@ var processors = [
 	easinggradient,
 	fontmagician,
 	fontresponsive
-]
-
+];
+// Dev Version - Required by "gulp"
 gulp.task('sassdev', function () {
 	return gulp.src('source/sass/**/*.scss')
 		.pipe(sourcemaps.init())
@@ -37,8 +38,9 @@ gulp.task('sassdev', function () {
 		.pipe(postcss(processors))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/css'))
-})
-
+});
+// Dist Version - Required by "gulp dist"
+// Compressed version without sourcemap
 gulp.task('sassprod', function () {
 	return gulp.src('source/sass/**/*.scss')
 		.pipe(sass({
@@ -46,11 +48,12 @@ gulp.task('sassprod', function () {
 		}).on('error', sass.logError))
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('dist/css'))
-})
+});
 
-///////////////////////////////////////////////////
-// Scripts
-///////////////////////////////////////////////////
+/**
+ * JavaScripts - Required by "gulp"
+ */
+// Dev Version - Required by "gulp"
 gulp.task('scripts', function () {
 	gulp.src('source/js/**/*.js')
 		.pipe(sourcemaps.init())
@@ -58,46 +61,68 @@ gulp.task('scripts', function () {
 		.pipe(concat('all.js'))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/js'))
-})
-
+});
+// Dist Version - Required by "gulp dist"
+// Compressed version without sourcemap
 gulp.task('scripts-dist', function () {
 	gulp.src('source/js/**/*.js')
 		.pipe(babel())
 		.pipe(concat('all.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/js'))
-})
+});
 
-///////////////////////////////////////////////////
-// Copy-HTML
-///////////////////////////////////////////////////
+/**
+ * HTML Copy - Required by "gulp"
+ */
 gulp.task('copy-html', function () {
 	gulp.src('source/*.html')
 		.pipe(gulp.dest('dist/'))
-})
+});
 
-///////////////////////////////////////////////////
-// HTML-Minify
-///////////////////////////////////////////////////
+/**
+ * Img Copy - Required by "gulp"
+ */
+gulp.task('copy-img', function () {
+	gulp.src('source/img/**/*')
+		.pipe(gulp.dest('dist/img'))
+});
+
+/**
+ * Watch Task - Required by "gulp"
+ */
+gulp.task('watch', function () {
+	gulp.watch('source/*.html', ['copy-html'])
+	gulp.watch('source/img/**/*', ['copy-img'])
+	gulp.watch('source/sass/**/*.scss', ['sassdev'])
+	gulp.watch('source/js/**/*.js', ['scripts'])
+});
+
+/**
+ * Default Task - "gulp"
+ */
+gulp.task('default', [
+	'copy-html',
+	'copy-img',
+	'sassdev',
+	'scripts',
+	'watch'
+]);
+
+/**
+ * HTML Minify - Required by "gulp dist"
+ */
 gulp.task('minify', function () {
 	return gulp.src('source/*.html')
 		.pipe(htmlmin({
 			collapseWhitespace: true
 		}))
 		.pipe(gulp.dest('dist'))
-})
+});
 
-///////////////////////////////////////////////////
-// Copy-IMG
-///////////////////////////////////////////////////
-gulp.task('copy-img', function () {
-	gulp.src('source/img/**/*')
-		.pipe(gulp.dest('dist/img'))
-})
-
-///////////////////////////////////////////////////
-// Image-Min
-///////////////////////////////////////////////////
+/**
+ * Image Resize Task - Required by "gulp dist"
+ */
 gulp.task('imgrsz', function () {
 	return gulp.src('source/img/**/*')
 		.pipe(imagemin({
@@ -105,29 +130,14 @@ gulp.task('imgrsz', function () {
 			use: [pngquant()]
 		}))
 		.pipe(gulp.dest('dist/img'))
-})
+});
 
-///////////////////////////////////////////////////
-// Dist
-///////////////////////////////////////////////////
+/**
+ * Dist Task - "gulp dist"
+ */
 gulp.task('dist', [
 	'minify',
 	'sassprod',
 	'scripts-dist',
 	'imgrsz'
-])
-
-///////////////////////////////////////////////////
-// Watch
-///////////////////////////////////////////////////
-gulp.task('watch', function () {
-	gulp.watch('source/*.html', ['copy-html'])
-	gulp.watch('source/img/**/*', ['copy-img'])
-	gulp.watch('source/sass/**/*.scss', ['sassdev'])
-	gulp.watch('source/js/**/*.js', ['scripts'])
-})
-
-///////////////////////////////////////////////////
-// Default
-///////////////////////////////////////////////////
-gulp.task('default', ['copy-html', 'copy-img', 'sassdev', 'scripts', 'watch'])
+]);
